bab9ca95ba579eb3096edfb3d0ec89b967a28592
Benjamin Christau <benjamin.christau@oyez.fr>
2016-09-14 18:17:55 +0200

    [Env] Setup web development environment.

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+Web :
+    [> CD Web]
+    
+    1) Install dependencies :
+        > CD config
+        > npm install
+    2) [Re]Create the bundle :
+        > CD ../js
+        > webpack
---
+++ Web/js/webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: './',
+        filename: "bundle.js"
+    },
+    devServer: {
+        inline: true,
+        contentBase: "./"
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    exclude: /node_modules/,
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("development")
+            }
+        })
+    ]
+};
