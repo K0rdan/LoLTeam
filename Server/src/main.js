e11eb2be495aac6e1140430a0144d63136b486de
K0rdan <christau_benjamin@hotmail.com>
2016-09-23 10:46:13 +0200

    [Server] First implementation of the Node server with Express + MySQL.

--- Server/src/main.js
+++ Server/src/main.js
@@ -1,19 +1,74 @@
-var app         = require('express');
-var mysql       = require('mysql');
-var connection  = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'cky_w+IQ@l',
-    database : 'db'
+// Lib Constant
+const express   = require('express');
+const mysql     = require('mysql');
+
+// Custom Constant 
+const PORT = 8000;
+const MYSQL_CONF = {
+    host        : 'lolteam-db',
+    localAddress: 'lolteam-db',
+    user        : 'root',
+    password    : 'cky_w+IQ@l',
+    database    : 'lolteam'
+};
+
+// Start
+var app = null, connection = null, isConnected = false;
+
+init();
+app.get("/",route_main);
+var server = app.listen(PORT, function() {
+  log('Server listening on port ' + server.address().port);
 });
 
-app.get("/",function(req,res){
-    connection.query('SELECT * from users', function(err, rows, fields) {
-    connection.end();
-    if (!err)
-        console.log('The solution is: ', rows);
+
+function getDate() {
+    var date = new Date();
+    var year = date.getFullYear(),
+        month= date.getMonth()+1,
+        day  = date.getDate(), 
+        hour = date.getHours(),
+        min  = date.getMinutes(),
+        sec  = date.getSeconds();
+    month = month < 9 ? "0"+month : month;
+
+    return year + "/" + month + "/" + day + ":" + hour + "-" + min + "-" + sec;
+}
+
+function log(type, msg) {
+    if(type.length < 6){
+        for(var i=0; type.length<6;i++)
+            type += " ";
+    }
     else
-        console.log('Error while performing Query.');
+        type = type.substr(0,6);
+
+    console.log("[" + getDate() + "][" + type + "] " + msg);
+}
+
+function init() {
+    app = express();
+    connection = mysql.createConnection(MYSQL_CONF);
+    connection.connect(function(err){
+        if(err){
+            log("MYSQL", "Error connecting to database : " + err);
+            return null; 
+        }
+
+        isConnected = true; 
     });
-});
- app.listen(3000);
+}
+
+function route_main(req, res) {    
+    if(isConnected) {
+        var query = "SELECT * FROM lolteam.users";
+        log("MYSQL","Query : '" + query + "'");
+
+        connection.query(query, function(err, rows, fields) {
+            if (!err)
+                console.log('The solution is: ', rows);
+            else
+                log("MYSQL", err);
+        });
+    }
+}
