d66d3654fb0f9912556f8fef5f352572e484424c
K0rdan <christau_benjamin@hotmail.com>
2016-09-23 10:48:02 +0200

    [Web][API] First connection to the server. TODO : externalization of the configuration.

--- Web/js/src/lib/api.js
+++ Web/js/src/lib/api.js
@@ -1,6 +1,21 @@
+const config = {
+    SERVER: '192.168.99.100',
+    PORT: '8000'
+};
+
 export default class API {
     constructor() {
-        
+        var req = new XMLHttpRequest();
+        req.open('GET', 'http://' + config.SERVER + ':' + config.PORT + '/', true);
+        req.onreadystatechange = function (e) {
+            if (req.readyState == 4) {
+                if(req.status == 200)
+                    console.log(req.responseText);
+                else
+                    console.log("Erreur pendant le chargement de la page.\n");
+            }
+        };
+        req.send(null);
     }
 
     login(username, password) {
